5e029bb9f80264213cf3dbf227e0c9e790f66365
Mees van Dongen <mvandongen@ilionx.com>
2022-10-13 14:54:43 +0200

    ci: rename semantic release file name

--- release.config.js
+++ release.config.js
@@ -1,12 +1,12 @@
-{
-  "branches": [
+module.exports = {
+  branches: [
     "master",
     {
-      "name": "next",
-      "prerelease": true
-    }
+      name: "next",
+      prerelease: true,
+    },
   ],
-  "plugins": [
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
@@ -15,14 +15,15 @@
     [
       "@semantic-release/git",
       {
-        "assets": [
+        assets: [
           "package.json",
           "package-lock.json",
           "./helm/values.yaml",
-          "CHANGELOG.md"
+          "CHANGELOG.md",
         ],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
-    ]
-  ]
-}
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
